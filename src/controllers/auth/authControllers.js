db0d2710b062cf11eed61f2694f6d2056c8ab8f8
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-17 20:56:19 +0300

    update: login controller

--- src/controllers/auth/authControllers.js
+++ src/controllers/auth/authControllers.js
@@ -1,5 +1,34 @@
-export const loginController = (req, res) => {
-  return res.json({ message: 'auth/login' });
+import dotenv from 'dotenv';
+import { sign } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../../db/User.js';
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const userSchema = new User({
+      userName: Math.random().toString(),
+      password: hashedPassword,
+      email,
+    });
+
+    const newUser = await userSchema.save();
+
+    console.log(newUser);
+
+    const token = await sign({ user: { email } }, secret);
+
+    res.status(200).json(token);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const registerController = (req, res) => {
